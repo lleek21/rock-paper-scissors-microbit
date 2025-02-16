480359f9a3c36bfe49dfc9012a34a611147c71cc
Lucy <lleek1791@gmail.com>
2025-02-16 20:57:31 +0000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,14 @@
+hand = 0
+
+def on_gesture_shake():
+    global hand 
+    hand = randint(1,3)
+    if hand == 1:
+        basic.show_icon(IconNames.SMALL_SQUARE)
+    elif hand == 2:
+        basic.show_icon(IconNames.SQUARE)
+    else:
+        basic.show_icon(IconNames.SCISSORS)
+    pass
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
